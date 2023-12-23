9fe8a95c26de11cf404f4e6033bdb67c9820dc2d
Arthur Koba <caprimores@gmail.com>
2023-12-24 02:17:32 +0300

    add wi-fi connect

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "configs.h"
+
+const char ssid[]  = "";
+const char pass[] = "";
 
 void setup() {
-// write your initialization code here
+    Serial.begin(SERIAL_BAUDRATE);
+    Serial.println(PSTR("\nStart system."));
+    pinMode(LED_BUILTIN, OUTPUT);
+    WiFi.begin(ssid, pass);
 }
 
 void loop() {
-// write your code here
+    if (WiFi.status() != WL_CONNECTED) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(100);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(500);
+    }
+    digitalWrite(LED_BUILTIN, LOW);
+
 }
