93fc15b0840011e4176ff960429be40f24fc4e96
Arthur Koba <caprimores@gmail.com>
2023-12-26 08:51:49 +0300

    add pwm out

--- src/core.h
+++ src/core.h
@@ -15,6 +15,29 @@
     #define FAST_BLINK_DELAY_MS 2000
 #endif
 
+#ifndef CH1_PIN
+    #error CH1_PIN not defined
+#endif
+
+#ifndef CH2_PIN
+    #error CH2_PIN not defined
+#endif
+
+#ifndef CH3_PIN
+    #error CH3_PIN not defined
+#endif
+
+#ifndef CH4_PIN
+    #error CH4_PIN not defined
+#endif
+
+#ifndef CH5_PIN
+    #error CH5_PIN not defined
+#endif
+
+enum Channels {
+    ONE = CH1_PIN, TWO = CH2_PIN, THREE = CH3_PIN, FOUR = CH4_PIN, FIVE = CH5_PIN
+};
 
 struct States {
     bool led : 1;
@@ -27,29 +50,30 @@ struct States {
     bool flag7 : 1;
 };
 
-struct Packet {
-    uint8_t packet_id;
-    uint8_t data_id;
-    uint16_t data_value;
+struct UpdatePacket {
+    uint8_t channel;
+    uint8_t bright;
 };
 
 class Core {
 public:
     Core() {
         pinMode(LED_PIN, OUTPUT);
+        pinMode(CH1_PIN, OUTPUT);
+        pinMode(CH2_PIN, OUTPUT);
+        pinMode(CH3_PIN, OUTPUT);
+        pinMode(CH4_PIN, OUTPUT);
+        pinMode(CH5_PIN, OUTPUT);
     }
 
     void data_handler(const uint8_t *data, size_t len) {
-//        if (data.length() != 4) return 0x4C45433F;
-//        Packet packet = *(Packet*)&data;
-//        Serial.print("\nLenght arg: ");
-//        Serial.println(data.length());
-//        Serial.print("packet_id : ");
-//        Serial.println(packet.packet_id);
-//        Serial.print("data_id: ");
-//        Serial.println(packet.data_id);
-//        Serial.print("data_value: ");
-//        Serial.println(packet.data_value);
+        uint8_t payload_ptr = *(data + 1);
+        if (data[0] == 50) {
+            UpdatePacket packet = *(UpdatePacket*)&payload_ptr;
+            switch (packet.channel)  {
+                case 1: case 2: case 3: case 4: case 5: write_bright(packet.channel, packet.bright); break;
+            }
+        }
     }
 
     void blink() {
@@ -66,6 +90,12 @@ public:
     }
 
 private:
+    static void write_bright(uint8_t pin, uint8_t value) {
+        float val = value;
+        val = (float)((val > 23) ? (0.00512 * val * val - 0.325 * val + 5.73) : 0);
+        analogWrite(pin, (uint8_t)val);
+    }
+
     uint32_t last_blink = 0;
     uint32_t last_fast_blink = 0;
     States states{};
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
 #include "configs.h"
 #include "core.h"
 #include "web_server.h"
