87da8d063ae1512b74791bffc3ea6b21d72ce813
Arthur Koba <caprimores@gmail.com>
2023-12-28 03:46:21 +0300

    add color modes `COLOR_MODE, FADE_MODE, SMOOTH_MODE`, change struct LightSource - add `color_mode_data` pointer

--- src/core_types.h
+++ src/core_types.h
@@ -3,8 +3,22 @@
 
 #include <Arduino.h>
 
-typedef enum {INIT_CHANNEL_OK = 0, INIT_CHANNEL_ERROR_LENGTH = 1} init_channel_status_t;
-typedef enum {INIT_SOURCES_OK = 0, INIT_SOURCES_ERROR_LENGTH = 1} init_sources_status_t;
+typedef enum {
+    INIT_CHANNEL_OK = 0,
+    INIT_CHANNEL_ERROR_LENGTH = 1
+} init_channel_status_t;
+
+typedef enum {
+    INIT_SOURCES_OK = 0,
+    INIT_SOURCES_ERROR_LENGTH = 1,
+    INIT_SOURCES_OK_WITH_NODES = 2
+} init_sources_status_t;
+
+typedef enum {
+    CHANGE_OK = 0,
+    CHANGE_PASS = 1
+} change_color_mode_source_status_t;
+
 typedef enum {
     CHANNEL_WRITE_OK = 0,
     CHANNEL_OUTSIDE,
@@ -14,6 +28,9 @@ typedef enum {
 
 enum ColorMode {
     OFF_MODE = 0,
+    COLOR_MODE = 1,
+    FADE_MODE,
+    SMOOTH_MODE,
 };
 
 struct States {
@@ -38,8 +55,8 @@ struct LightSource {
     Channel *ch1 = nullptr;
     Channel *ch2 = nullptr;
     Channel *ch3 = nullptr;
-    uint8_t state = 0;
     ColorMode mode = OFF_MODE;
+    uint8 *color_mode_data = nullptr;
 };
 
 #endif //ESP8266_FAIRY_LIGHTS_CORE_TYPES_H
