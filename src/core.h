10644fdcd38ed89c0066596ee3b7901ee1da36ae
Arthur Koba <caprimores@gmail.com>
2023-12-30 14:54:17 +0300

    refactor core for use light sources

--- src/core.h
+++ src/core.h
@@ -13,111 +13,67 @@
 #define UPDATE_CORE_DELAY 5
 #endif
 
-#include "core_types.h"
-#include "color_modes.h"
-//#include "web_server_types.h"
+#include "web_server_types.h"
+#include "light_source.h"
+
+typedef enum {
+    INIT_SOURCES_OK = 0,
+    INIT_SOURCES_FAIL_ALREADY_ADDED
+} init_sources_status_t;
+
+typedef enum {
+    CHANGE_MODE_OK = 0,
+    CHANGE_MODE_FAIL_OUTSIDE_SOURCE = 1,
+    CHANGE_MODE_FAIL_OUTSIDE_MODE = 2
+} change_mode_status_t;
+
+
+class SourceItem {
+public:
+    explicit SourceItem(Source &_source) : next(nullptr), source(_source) {}
+    SourceItem *next = nullptr;
+    Source &source;
+};
 
 
 class Core {
 public:
-    Core() = default;
-
-    write_channel_status_t write_channel(uint8_t channel, uint8_t val) {
-        if (channel > channels_length) return CHANNEL_OUTSIDE;
-        if (channels == nullptr) return NOT_INITED_CHANNELS;
-        if (channels[channel].is_not_inited) return CHANNEL_NOT_INITED;
-        channels[channel].bright = val;
-        return CHANNEL_WRITE_OK;
+    Core() : first(nullptr), last(nullptr) {};
+    ~Core() {
+        SourceItem *p = first;
+        SourceItem *deleted = nullptr;
+        while (p) {
+            deleted = p;
+            p = p->next;
+            delete deleted;
+        };
     }
 
-    init_channel_status_t init_channels(Channel *channels_ptr, uint8_t len) {
-        if (len < 1) return init_channel_status_t::INIT_CHANNEL_ERROR_LENGTH;
-        channels = channels_ptr;
-        channels_length = len;
-        for (int i = 0; i < len; ++i) {
-            if (channels[i].pin < 0) continue;
-            pinMode(channels[i].pin, OUTPUT);
-            channels[i].is_not_inited = false;
-            write_bright_CRT(channels[i]);
+    init_sources_status_t add_light_source(Source &source) {
+        auto* new_source_item_p = new SourceItem(source);
+        if (first == nullptr) {
+            first = new_source_item_p;
+            last = new_source_item_p;
+            return INIT_SOURCES_OK;
         }
-        return init_channel_status_t::INIT_CHANNEL_OK;
+        SourceItem *p = first;
+        while (p) {
+            if (&p->source == &source) return INIT_SOURCES_FAIL_ALREADY_ADDED;
+            p = p->next;
+        };
+        last->next = new_source_item_p;
+        last = new_source_item_p;
+        return init_sources_status_t::INIT_SOURCES_OK;
     }
 
-    init_sources_status_t init_led_source(LightSource *sources_ptr, uint8_t len) {
-        if (len < 1) return init_sources_status_t::INIT_SOURCES_ERROR_LENGTH;
-        init_sources_status_t status = init_sources_status_t::INIT_SOURCES_OK;
-        sources = sources_ptr;
-        sources_length = len;
-        for (int i = 0; i < len; ++i) {
-            if (change_color_mode_source(sources[i], sources[i].mode) == CHANGE_OK) continue;
-            status = INIT_SOURCES_OK_WITH_NODES;
+    void show_modes() {
+        if (millis() - last_update < UPDATE_CORE_DELAY) return;
+        SourceItem *p = first;
+        while (p) {
+            p->source.show_mode();
+            p = p->next;
         }
-        return status;
-    }
-
-    static change_color_mode_source_status_t change_color_mode_source(LightSource &source, ColorMode new_color_mode) {
-        if (source.mode == new_color_mode && source.color_mode_data != nullptr)
-            return change_color_mode_source_status_t::CHANGE_PASS;
-        delete[] source.color_mode_data;
-        switch (new_color_mode) {
-            case OFF_MODE:
-                source.mode = OFF_MODE;
-                break;
-            case COLOR_MODE:
-                source.mode = COLOR_MODE;
-                break;
-            case SMOOTH_MODE:
-                source.mode = SMOOTH_MODE;
-                source.color_mode_data = reinterpret_cast<uint8_t*>(new SmoothModeData{});
-                break;
-            case FADE_MODE:
-                source.mode = FADE_MODE;
-                source.color_mode_data = reinterpret_cast<uint8_t*>(new FadeModeData{});
-                break;
-        }
-        return change_color_mode_source_status_t::CHANGE_OK;
-    }
-
-    static void show_color_mode_source(LightSource &source) {
-        ChannelCount len = source.ch2 == nullptr ? ChannelCount::ONE : source.ch3 == nullptr ? ChannelCount::TWO : ChannelCount::THREE;
-        ChannelsBright bright;
-        bool need_update = false;
-        switch (source.mode) {
-            case OFF_MODE:
-            case COLOR_MODE:
-                switch (len) {
-                    case THREE: bright.ch3 = source.ch3->bright;
-                    case TWO  : bright.ch2 = source.ch2->bright;
-                    case ONE  : bright.ch1 = source.ch1->bright;
-                }
-                need_update = true;
-                break;
-            case SMOOTH_MODE:
-                need_update = ColorModes::calculate_smooth_color_mode(
-                        bright, *reinterpret_cast<SmoothModeData*>(source.color_mode_data)
-                );
-                break;
-            case FADE_MODE:
-
-                need_update = ColorModes::calculate_fade_color_mode(
-                        bright, *reinterpret_cast<FadeModeData*>(source.color_mode_data)
-                );
-                break;
-        }
-        if (!need_update) return;
-        switch (len) {
-            case THREE: write_bright_CRT(source.ch3->pin, bright.ch3);
-            case TWO  : write_bright_CRT(source.ch2->pin, bright.ch2);
-            case ONE  : write_bright_CRT(source.ch1->pin, bright.ch1);;
-        }
-    }
-
-    static void write_bright_CRT(Channel &channel) { write_bright_CRT(channel.pin, channel.bright); }
-
-    static void write_bright_CRT(uint8_t pin, uint8_t val) {
-//        uint8_t bright = ((long)val * val + 255) >> 8;
-        uint16_t bright = pgm_read_word(&(crt_gamma_pgm_10_bit[val]));
-        analogWrite(pin, bright);
+        last_update = millis();
     }
 
     void blink() {
@@ -126,25 +82,49 @@ public:
         digitalWrite(LED_PIN, !digitalRead(LED_PIN));
     }
 
-    void update() {
-        if (millis() - last_update < UPDATE_CORE_DELAY) return;
-        for (int i = 0; i < sources_length; ++i) show_color_mode_source(sources[i]);
-        last_update = millis();
+    size_t get_size_buffer_all_sources() {
+        size_t count = 0;
+        SourceItem *p = first;
+        while (p) {
+            count += p->source.get_name().length() + 2;
+            p = p->next;
+        }
+        return count;
     }
 
-    void write_channels_bright_to_buffer(uint8_t *buffer) {
-        for (int i = 0; i < channels_length; ++i) {buffer[i] = channels[i].bright; }
+    Source* get_source(uint8_t id) {
+        SourceItem *p = first;
+        uint8_t count = 0;
+        while (p) {
+            if (id == count++) return &p->source;
+            p = p->next;
+        }
+        return nullptr;
     }
 
-    [[nodiscard]] uint8_t get_len_channels() const {
-        return channels_length;
+    void write_sources_info_to_buffer(uint8_t *buffer) {
+        SourceItem *p = first;
+        uint16_t offset = 0;
+        while (p) {
+            buffer[offset++] = p->source.get_mode();
+            String &name = p->source.get_name();
+            buffer[offset++] = name.length();
+            for (int i = 0; i < name.length(); ++i) {buffer[offset++] = name.charAt(i);}
+            p = p->next;
+        }
+    }
+
+    change_mode_status_t change_mode(uint8_t source_id, uint8_t mode) {
+        Source *source = get_source(source_id);
+        if (!source) return change_mode_status_t::CHANGE_MODE_FAIL_OUTSIDE_SOURCE;
+        if (mode >= ColorMode::UNKNOWN_MODE) return change_mode_status_t::CHANGE_MODE_FAIL_OUTSIDE_MODE;
+        source->change_color_mode_source(static_cast<ColorMode>(mode));
+        return change_mode_status_t::CHANGE_MODE_OK;
     }
 
 private:
-    LightSource *sources = nullptr;
-    Channel *channels = nullptr;
-    uint8_t channels_length = 0;
-    uint8_t sources_length = 0;
+    SourceItem *first = nullptr;
+    SourceItem *last = nullptr;
     uint32_t last_blink = 0;
     uint32_t last_update = 0;
 };
