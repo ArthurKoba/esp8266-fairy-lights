5052eb9a11163e870403ce4e747c15c447b600bf
Arthur Koba <caprimores@gmail.com>
2023-12-30 14:55:16 +0300

    use light sources | refactor web server for this

--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,39 @@
 #include "configs.h"
 #include "core.h"
 #include "web_server.h"
+#include "light_source.h"
 
 const char ssid[]  = "";
 const char pass[] = "";
 WebServer server;
 Core core;
 
-
-Channel channels[9] = {
-        {.pin = 5},
-        {.pin = 4},
-        {.pin = 0},
-
-        {.pin = 2},
-        {.pin = 14},
-
-        {.pin = 12},
-        {.pin = 13},
-        {.pin = 15},
-};
-
-LightSource sources[3] = {
-        {&channels[0], &channels[1], &channels[2], SMOOTH_MODE},
-        {&channels[3], &channels[4], nullptr, OFF_MODE},
-        {&channels[5], &channels[6], &channels[7], FADE_MODE},
-};
+Source source_one = Source("Fairy Light One");
+Source source_two = Source("Fairy Light Two");
+Source source_three = Source("Led Strip");
 
 void setup() {
     Serial.begin(SERIAL_BAUDRATE);
     Serial.println(PSTR("\nStart system."));
-
+    pinMode(LED_PIN, OUTPUT);
     analogWriteResolution(10);
 
-    core.init_channels(channels, 5);
-    core.init_led_source(sources, 3);
-    pinMode(LED_PIN, OUTPUT);
+    source_one.add_channel(4);
+    source_one.add_channel(0);
 
+    source_two.add_channel(2);
+    source_two.add_channel(14);
+    source_two.change_color_mode_source(ColorMode::SMOOTH_MODE);
+
+    source_three.add_channel(12);
+    source_three.add_channel(13);
+    source_three.add_channel(15);
+
+    Serial.println(PSTR("Channels inited."));
+    core.add_light_source(source_one);
+    core.add_light_source(source_two);
+    core.add_light_source(source_three);
+    Serial.println(PSTR("Sources inited. Connect Wi-Fi..."));
     WiFi.begin(ssid, pass);
     wl_status_t status;
     do {
@@ -47,9 +44,15 @@ void setup() {
         status = WiFi.status();
         delay(100);
     } while (status != WL_CONNECTED);
-    digitalWrite(LED_PIN, true);
+
+    Serial.print(PSTR("Wi-Fi connected! IP: "));
+    Serial.println(WiFi.localIP());
+    digitalWrite(LED_PIN, false);
 
     server.init(&core);
+    Serial.println(PSTR("Server successful inited! Start modes."));
 }
 
-void loop() {core.update();}
+void loop() {
+    core.show_modes();
+}
--- src/web_server.h
+++ src/web_server.h
@@ -4,7 +4,6 @@
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
-#include "core_types.h"
 #include "web_server_types.h"
 #include "web_server_files/files.h"
 
@@ -19,41 +18,39 @@ public:
 #warning "CORS ENABLED!"
 #endif
         c = core;
-        srv.on("/updateChannel", HTTP_POST,
-               [] (Req *r) {},
-               nullptr,
-               [this] (Req *r, const uint8_t *data, size_t len, size_t index, size_t total) {
-            update_channel_handler(r, data, len);
+        srv.on("/changeMode", HTTP_POST, [] (Req *r) {},nullptr,
+       [this] (Req *r, const uint8_t *d, size_t l, size_t i, size_t t) {change_mode(r, d, l);
         });
-        srv.on("/getChannelsBright", HTTP_GET, [this] (Req *r) { get_channels(r);});
+        srv.on("/getSources", HTTP_GET, [this] (Req *r) { get_sources(r);});
         srv.on("/", HTTP_GET, [this] (Req *r) {page_handler(r, web_server_file_t::INDEX);});
         srv.on("/styles.css", HTTP_GET, [this] (Req *r) {page_handler(r, web_server_file_t::STYLES);});
         srv.on("/app.js", HTTP_GET, [this] (Req *r) {page_handler(r, web_server_file_t::APP_JS);});
-        srv.onNotFound( [this] (Req *r) {page_handler(r, web_server_file_t::NOT_FOUND_HTML);});
-
+        srv.onNotFound( [] (Req *r) {page_handler(r, web_server_file_t::NOT_FOUND_HTML);});
         srv.begin();
     }
 
 protected:
-    void get_channels(Req *r) {
-        auto *brights = new uint8_t[c->get_len_channels()];
-        c->write_channels_bright_to_buffer(brights);
-        r->send_P(200, OCTET_TYPE, brights, c->get_len_channels());
-        delete[] brights;
+    void change_mode(Req *r, const uint8_t *data, size_t len) {
+        if (len != 2) return r->send_P(500, PLAIN_TYPE, PSTR("Length not equals 2 byte"));
+        uint8_t source_id = data[0];
+        uint8_t mode = data[1];
+        change_mode_status_t status = c->change_mode(source_id, mode);
+        switch (status) {
+            case CHANGE_MODE_OK:
+                return r->send_P(200, PLAIN_TYPE, PSTR("OK"));
+            case CHANGE_MODE_FAIL_OUTSIDE_SOURCE:
+                return r->send_P(500, PLAIN_TYPE, PSTR("Source outsize"));
+            case CHANGE_MODE_FAIL_OUTSIDE_MODE:
+                return r->send_P(500, PLAIN_TYPE, PSTR("Mode outsize"));
+        }
     }
 
-    void update_channel_handler(Req *r, const uint8_t *data, size_t len) {
-        if (sizeof(UpdateChannelPacket) != len) {
-            return r->send_P(406, PLAIN_TYPE, PSTR("Packet length error"));
-        }
-        UpdateChannelPacket packet = *(UpdateChannelPacket*)data;
-        write_channel_status_t status = c->write_channel(packet.channel, packet.bright);
-        switch (status) {
-            case CHANNEL_WRITE_OK: r->send_P(200, PLAIN_TYPE, PSTR("OK")); break;
-            case CHANNEL_OUTSIDE: r->send_P(500, PLAIN_TYPE, PSTR("Channel beyond initialization channels")); break;
-            case CHANNEL_NOT_INITED: r->send_P(500, PLAIN_TYPE, PSTR("Channel not inited if firmware")); break;
-            case NOT_INITED_CHANNELS: r->send_P(500, PLAIN_TYPE, PSTR("Firmware error. Channels not inited in core.init()")); break;
-        }
+    void get_sources(Req *r) {
+        size_t buffer_size = c->get_size_buffer_all_sources();
+        auto *data = new uint8_t[buffer_size];
+        c->write_sources_info_to_buffer(data);
+        r->send_P(200, OCTET_TYPE, data, buffer_size);
+        delete[] data;
     }
 
     static void page_handler(Req *r, web_server_file_t file) {
