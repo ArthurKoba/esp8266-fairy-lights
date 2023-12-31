4624950bcadb4a5924f69ea40a0b45ae7fe61b49
Arthur Koba <caprimores@gmail.com>
2023-12-31 10:44:41 +0300

    refactor core, add change bright, get sources infos

--- src/core.h
+++ src/core.h
@@ -27,6 +27,12 @@ typedef enum {
     CHANGE_MODE_FAIL_OUTSIDE_MODE = 2
 } change_mode_status_t;
 
+typedef enum {
+    CHANGE_BRIGHT_OK = 0,
+    CHANGE_BRIGHT_FAIL_OUTSIDE_SOURCE = 1,
+    CHANGE_BRIGHT_FAIL_OUTSIDE_CHANNEL = 2
+} change_bright_status_t;
+
 
 class SourceItem {
 public:
@@ -51,6 +57,10 @@ public:
 
     init_sources_status_t add_light_source(Source &source) {
         auto* new_source_item_p = new SourceItem(source);
+        count_sources++;
+        count_chars_info_all_channels += source.get_count_chars_channels_info();
+        count_all_channels += source.get_count_channels();
+        count_chars_info_all_sources += source.get_name().length();
         if (first == nullptr) {
             first = new_source_item_p;
             last = new_source_item_p;
@@ -83,14 +93,35 @@ public:
         digitalWrite(LED_PIN, !digitalRead(LED_PIN));
     }
 
-    size_t get_size_buffer_all_sources() {
-        size_t count = 0;
+    size_t get_source_info_buffer_length() const {
+        // count sources (byte) + sources * 2 (modes + channels) + channels * 2 (bright and len chars) + chars
+        return 1 + count_sources * 3 + count_chars_info_all_sources + count_all_channels * 2 + count_chars_info_all_channels;
+    }
+
+    size_t get_sources_info(uint8_t *buffer) {
+        if (!count_sources) return 0;
+        size_t size = 0;
+        buffer[size++] = count_sources; // set count sources
         SourceItem *p = first;
         while (p) {
-            count += p->source.get_name().length() + 2;
+            Source *source = &p->source;
+            buffer[size++] = source->get_mode(); // set source mode
+            buffer[size++] = source->get_name().length(); // set source mode
+            for (int i = 0; i < source->get_name().length(); ++i) { // write source name
+                buffer[size++] = source->get_name().c_str()[i];
+            }
+            buffer[size++] = source->get_count_channels(); // set count channels in source
+            for (int i = 0; i < source->get_count_channels(); ++i) {
+                Channel *ch = source->get_channel(i);
+                buffer[size++] = ch->bright; // set bright
+                buffer[size++] = ch->name.length(); // set count chars in name channel
+                for (int j = 0; j < ch->name.length(); ++j) {
+                    buffer[size++] = ch->name.c_str()[j]; // write name
+                }
+            }
             p = p->next;
         }
-        return count;
+        return size;
     }
 
     Source* get_source(uint8_t id) {
@@ -103,31 +134,32 @@ public:
         return nullptr;
     }
 
-    void write_sources_info_to_buffer(uint8_t *buffer) {
-        SourceItem *p = first;
-        uint16_t offset = 0;
-        while (p) {
-            buffer[offset++] = p->source.get_mode();
-            String &name = p->source.get_name();
-            buffer[offset++] = name.length();
-            for (int i = 0; i < name.length(); ++i) {buffer[offset++] = name.charAt(i);}
-            p = p->next;
-        }
-    }
-
     change_mode_status_t change_mode(uint8_t source_id, uint8_t mode) {
         Source *source = get_source(source_id);
         if (!source) return change_mode_status_t::CHANGE_MODE_FAIL_OUTSIDE_SOURCE;
         if (mode >= ColorMode::UNKNOWN_MODE) return change_mode_status_t::CHANGE_MODE_FAIL_OUTSIDE_MODE;
-        source->change_color_mode_source(static_cast<ColorMode>(mode));
+        source->set_mode(static_cast<ColorMode>(mode));
         return change_mode_status_t::CHANGE_MODE_OK;
     }
+
+    change_bright_status_t change_bright(uint8_t source_id, uint8_t channel_id, uint8_t bright) {
+        Source *source = get_source(source_id);
+        if (!source) return change_bright_status_t::CHANGE_BRIGHT_FAIL_OUTSIDE_SOURCE;
+        if (!source->set_bright(channel_id, bright))
+            return change_bright_status_t::CHANGE_BRIGHT_FAIL_OUTSIDE_CHANNEL;
+        source->set_mode(ColorMode::COLOR_MODE);
+        return change_bright_status_t::CHANGE_BRIGHT_OK;
+    };
 
 private:
     SourceItem *first = nullptr;
     SourceItem *last = nullptr;
     uint32_t last_blink = 0;
     uint32_t last_update = 0;
+    uint16_t count_chars_info_all_channels = 0;
+    uint16_t count_chars_info_all_sources = 0;
+    uint8_t count_all_channels = 0;
+    uint8_t count_sources = 0;
 };
 
 #endif //ESP8266_FAIRY_LIGHTS_CORE_H
